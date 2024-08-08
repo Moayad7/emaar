4de1486c068f6c54cd20bf11c1b22a4326172524
Moayad7 <mouayad.ahmad7@gmail.com>
2024-08-08 13:25:53 +0300

    س

--- js/homePage.js
+++ js/homePage.js
@@ -21,8 +21,8 @@ servicesList.innerHTML = `
 
 services.innerHTML = `
   ${services_List.map((item) => `
-             <a href="servicesItem.html" class="swiper-slide bg-[url('assets/images/services/${item}')] bg-cover bg-center" data-aos="fade-up"
-        data-aos-duration="1000">
+             <a href="servicesItem.html" class="swiper-slide bg-cover bg-center" data-aos="fade-up"
+        data-aos-duration="1000" style="background-image: url('assets/images/services/${item}.jpg');">
           <div class="bg-[#082b3f88] hover:bg-[#082b3fcc] duration-500 h-[350px] flex justify-center items-center">
             <div class="text-center flex flex-col gap-2">
               <h2 class="text-xl lg:text-3xl font-semibold text-[#d6e1e5]">${item}</h2>
