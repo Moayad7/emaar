8b70fa22fef33b9c7f53a91d10d6511c5133f228
Moayad7 <mouayad.ahmad7@gmail.com>
2024-08-08 13:46:30 +0300

    Update servicesPage.js

--- js/servicesPage.js
+++ js/servicesPage.js
@@ -14,7 +14,7 @@ servicesList.innerHTML = `
   ${services_List
     .map(
       (item) => `
-             <div class="bg-[url('assets/images/services')] bg-center bg-cover h-96 w-full lg:w-72 group">
+             <div class=" bg-center bg-cover h-96 w-full lg:w-72 group" style="background-image: url('assets/images/services/${item}.jpg');">
                     <div class="lg:bg-[#082b3f99] bg-[#082b3faa] duration-200 lg:group-hover:bg-[#082b3faa] text-[#d6e1e5] text-center h-full w-full flex flex-col justify-center items-center gap-4">
                         <p class="text-4xl lg:opacity-75 lg:group-hover:opacity-100 duration-300 lg:translate-y-12 lg:group-hover:translate-y-0">${item} </p>
                         <p class="lg:opacity-0 lg:group-hover:opacity-100 duration-500 lg:translate-y-12 lg:group-hover:translate-y-0"> خدماتنا  تشمل الكثير ...</p>
